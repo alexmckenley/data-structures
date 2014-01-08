877d0f4be7493ddb8c3b1361ceaa3fefd43ece2f
Hack Reactor Students <alexmckenley@gmail.com>
2014-01-08 11:26:04 -0800

    begin work on functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,21 +1,25 @@
 var makeStack = function(){
-    var instance = {};
+  var instance = {};
 
   // Use an object with numeric keys to store values
-    var storage = {};
-  /* START PROMPT
-    var size; // Hint: set an initial value here
-  -END PROMPT */
+  var storage = {};
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   instance.push = function(value){
+    storage[size] = value;
+    size += 1;
   };
 
   instance.pop = function(){
+    return storage[size - 1];
+    delete storage[size - 1];
+    size -= 1;
   };
 
   instance.size = function(){
+    return size;
   };
-  
-    return instance;
+
+  return instance;
   };
