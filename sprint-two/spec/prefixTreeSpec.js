b844bc5af9d96174b4e3e619624bf0a54278f1b7
Hack Reactor Students <alexmckenley@gmail.com>
2014-01-11 11:09:12 -0800

    PrefixTree insert method implemented

--- sprint-two/spec/prefixTreeSpec.js
+++ sprint-two/spec/prefixTreeSpec.js
@@ -5,10 +5,25 @@ describe("prefixTree", function() {
 
   beforeEach(function() {
     pt = new PrefixTree();
+    var words = JSON.parse($('#words').html());
+    //insert words into tree
+    console.log(words);
+    for (var key in words){
+      pt.insert(key, words[key]);
+    }
+
+    // _.each(words, function(v, k, obj){
+    //   debugger;
+    //   pt.insert(obj[k], k);
+    // });
   });
 
   it("should have a method named 'insert'", function() {
     expect(pt.insert).to.be.a('function');
+  });
+
+  it("should retreive the best prediction given a string", function(){
+    expect(pt.predict("th").to.equal("the"));
   });
   
 });
--- sprint-two/src/prefixTree.js
+++ sprint-two/src/prefixTree.js
@@ -1,12 +1,35 @@
-var PrefixTree = function(){
-  this._children = [];
+var PrefixTree = function(value, rank, isWord){
+  this._children = {};
+  this._rank = rank || null;
+  this._isWord = isWord || false;
+  this._value = value || "";
 
   //load elements as a test
-  var test = JSON.parse($('#words').html());
-  console.log(test);
 
 };
 
-PrefixTree.prototype.insert = function(value){
+PrefixTree.prototype.insert = function(word, rank){
+  word = word.split("");
+  var count = 0;
+  var wordCount = word.length;
 
+  var recurse = function(startNode){
+    if(count === word.length){
+      return;
+    }
+    if (rank < startNode._rank){
+        startNode._rank = rank;
+      }
+    if(!startNode._children[word[count]]){
+      var isWord = word.length - 1 === count;
+      startNode._children[word[count]] = new PrefixTree(word[count], rank, isWord);
+      count += 1;
+      recurse(startNode._children[word[count-1]]);
+    } else {
+      count += 1;
+      recurse(startNode._children[word[count-1]]);
+    }
+  };
+
+  recurse(this);
 };
