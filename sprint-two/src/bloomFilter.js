4d5a13df03cad7fe0debd916e1ce294e47eb05f3
Alex Taylor <alexmckenley@gmail.com>
2014-01-12 19:50:11 -0800

    resolved bitwise math error

--- sprint-two/src/bloomFilter.js
+++ sprint-two/src/bloomFilter.js
@@ -18,16 +18,16 @@ BloomFilter.prototype.insert = function(key, value){
   // console.log("element value=", this._storage[Math.floor((i1 % this._limit)/8)].toString(2));
   // console.log("or'ing with=", Math.pow(2, ( (i1 % this._limit) - (8 * element )) - 1).toString(2));
 
-  element = ((i1 % this._limit)/8) === 1 ? Math.floor((i1 % this._limit)/8) - 1 : Math.floor((i1 % this._limit)/8);
-  this._storage[element] = this._storage[element] | Math.pow(2, ( (i1 % this._limit) - (8 * element)) - 1 );
-  element = ((i2 % this._limit)/8) === 1 ? Math.floor((i2 % this._limit)/8) - 1 : Math.floor((i2 % this._limit)/8);
-  this._storage[element] = this._storage[element] | Math.pow(2, ( (i2 % this._limit) - (8 * element)) - 1 );
-  element = ((i3 % this._limit)/8) === 1 ? Math.floor((i3 % this._limit)/8) - 1 : Math.floor((i3 % this._limit)/8);
-  this._storage[element] = this._storage[element] | Math.pow(2, ( (i3 % this._limit) - (8 * element)) - 1 );
+  element = Math.floor((i1 % this._limit)/8);
+  this._storage[element] = this._storage[element] | Math.pow(2, ( (i1 % this._limit) - (8 * element)) );
+  element = Math.floor((i2 % this._limit)/8);
+  this._storage[element] = this._storage[element] | Math.pow(2, ( (i2 % this._limit) - (8 * element)) );
+  element = Math.floor((i3 % this._limit)/8);
+  this._storage[element] = this._storage[element] | Math.pow(2, ( (i3 % this._limit) - (8 * element)) );
   
   this._actualStorage[key] = value;
 
-  console.log(this._storage[0].toString(2).split("").reverse().join(""), this._storage[1].toString(2).split("").reverse().join(""), this._storage[2].toString(2).split("").reverse().join(""));
+  //console.log(this._storage[0].toString(2).split("").reverse().join(""), this._storage[1].toString(2).split("").reverse().join(""), this._storage[2].toString(2).split("").reverse().join(""));
 };
 
 BloomFilter.prototype.retrieve = function(key){
@@ -35,11 +35,11 @@ BloomFilter.prototype.retrieve = function(key){
   var i2 = murmurhash3_32_gc(key, 78945641);
   var i3 = murmurhash3_32_gc(key, 123456789);
 
-  var element = ((i1 % this._limit)/8) === 1 ? Math.floor((i1 % this._limit)/8) - 1 : Math.floor((i1 % this._limit)/8);
+  var element = Math.floor((i1 % this._limit)/8);
   var val1 = this._storage[element] & Math.pow(2, ( (i1 % this._limit) - (8 * element)) - 1 );
-  element = ((i2 % this._limit)/8) === 1 ? Math.floor((i2 % this._limit)/8) - 1 : Math.floor((i2 % this._limit)/8);
+  element = Math.floor((i2 % this._limit)/8);
   var val2 = this._storage[element] & Math.pow(2, ( (i2 % this._limit) - (8 * element)) - 1 );
-  element = ((i3 % this._limit)/8) === 1 ? Math.floor((i3 % this._limit)/8) - 1 : Math.floor((i3 % this._limit)/8);
+  element = Math.floor((i3 % this._limit)/8);
   var val3 = this._storage[element] & Math.pow(2, ( (i3 % this._limit) - (8 * element)) - 1 );
   //console.log(val1, val2, val3);
 
